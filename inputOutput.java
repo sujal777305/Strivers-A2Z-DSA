7067c6e7839b884f081f7c355406300637c3f41b
sujal777305 <sujalpatel01234@gmail.com>
2025-08-20 23:29:56 +0530

    Add files via upload

---
+++ inputOutput.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class inputOutput {
+    public static void main(String args[]) {
+        Scanner obj = new Scanner(System.in);
+        int a = obj.nextInt();
+        double b = obj.nextDouble();
+        System.out.println("a : " + a);
+        System.out.println("b : " + b);
+    }
+}
