cf427cb72180fd5510b552d267c4bdda0c8de4e4
sujal777305 <sujalpatel01234@gmail.com>
2025-10-01 15:54:58 +0530

    Add files via upload

---
+++ findMissingNumber.java
@@ -0,0 +1,32 @@
+import java.util.*;
+class findMissingNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the array : ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        System.out.println("Enter the elements of the array : ");
+        for (int i=0; i<n; i++) {
+            arr[i] = sc.nextInt();
+        }   
+
+        for(int i=0; i<n-1; i++) {
+            for(int j=0; j<n-i-1; j++) {
+                if(arr[j] > arr[j+1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+
+        int i;
+        for(i=0; i<n; i++) {
+            if(arr[i] != i) {
+                break;
+            }
+        }
+        System.out.println("The missing number is : " + i);   
+    }
+}
